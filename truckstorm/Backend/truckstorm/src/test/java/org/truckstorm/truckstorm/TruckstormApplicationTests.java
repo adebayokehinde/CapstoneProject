89ebe02fc346a2e29dee38212c9e4761c0fcf3ae
tecmalik <abdulmalikojo2@gmail.com>
2025-07-13 21:19:36 +0100

    prepairing apstone env

---
+++ truckstorm/Backend/truckstorm/src/test/java/org/truckstorm/truckstorm/TruckstormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.truckstorm.truckstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckstormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
