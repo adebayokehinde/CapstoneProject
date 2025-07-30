c986163fefa73df021d85407bffced046ec35562
tecmalik <abdulmalikojo2@gmail.com>
2025-07-30 11:42:32 +0100

    Removing security teplorarily

--- truckstorm/Backend/truckstorm/src/main/java/com/example/truckstorm/util/JwtKeyGenerator.java
+++ truckstorm/Backend/truckstorm/src/main/java/com/example/truckstorm/util/JwtKeyGenerator.java
@@ -7,11 +7,11 @@ import javax.crypto.SecretKey;
 import java.util.Base64;
 
 public class JwtKeyGenerator {
-    public static void main(String[] args) {
-
-        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
-        String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
-        System.out.println("Generated Key: " + base64Key);
-    }
+//    public static void main(String[] args) {
+//
+//        SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//
+//        String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
+//        System.out.println("Generated Key: " + base64Key);
+//    }
 }
