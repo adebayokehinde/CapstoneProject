e09d87e8a9ab132f46e50194da2cbf2b40bc4a95
tecmalik <abdulmalikojo2@gmail.com>
2025-07-31 08:51:43 +0100

    restructuring settings

--- truckstorm/src/main/java/com/example/truckstorm/TruckStormApplication.java
+++ truckstorm/src/main/java/com/example/truckstorm/TruckStormApplication.java
@@ -2,8 +2,12 @@ package com.example.truckstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.truckstorm.data.repository")
+@EntityScan("com.example.truckstorm.data.model")
 public class TruckStormApplication {
 
     public static void main(String[] args) {
